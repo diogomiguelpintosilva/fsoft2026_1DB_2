7012f0b2e7b94fffd32bcfbdbc557ed79be4fdf9
diogosilva <1250882@isep.ipp.pt>
2026-05-14 12:35:56 +0100

    Header das Transações

---
+++ Transacoes/Transacoes.h
@@ -0,0 +1,39 @@
+#ifndef PROJETOFSOFT_TRANSACOES_H
+#define PROJETOFSOFT_TRANSACOES_H
+
+#include <string>
+#include <vector>
+#include <ctime>
+
+class Transacoes {
+
+    public:
+
+    struct Registo {
+
+        std::string descricao;
+        double valor;
+        std::string data;
+    };
+
+    Transacoes();
+    virtual ~Transacoes() = default;
+
+    void registar(const std::string& descricao, double valor);
+
+    const std::vector<Transacoes::Registo> &getHistorico() const;
+
+    const std::vector<Registo>& getRegistars() const;
+    void mostrarHistorico() const;
+
+    virtual void adicionarAoHistorico() = 0;
+
+
+protected:
+
+    std::vector<Registo> historico;
+
+    static std::string dataAtual();
+};
+
+#endif //PROJETOFSOFT_TRANSACOES_H
