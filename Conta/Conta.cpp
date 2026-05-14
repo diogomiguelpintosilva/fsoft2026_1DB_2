fd333d5a5d69f47fa888781b0e2d05778b4086bf
diogosilva <1250882@isep.ipp.pt>
2026-05-14 12:36:50 +0100

    Sources da Conta

---
+++ Conta/Conta.cpp
@@ -0,0 +1,31 @@
+#include "Conta.h"
+#include <iostream>
+#include <iomanip>
+
+
+Conta::Conta(const std::string& numeroConta, const std::string& pin)
+    : numeroConta(numeroConta), pin(pin), saldo(0) {}
+
+
+const std::string& Conta::getNumeroConta() const {
+
+    return numeroConta;
+}
+
+
+double Conta::getSaldo() const {
+
+    return saldo;
+}
+
+
+bool Conta::verificarPin(const std::string& pinIntroduzi) const {
+
+    return pin == pinIntroduzi;
+}
+
+
+void Conta::setSaldo(double novoSaldo) {
+
+    saldo = novoSaldo;
+}
