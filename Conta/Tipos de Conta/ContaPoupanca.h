13a848ab40f1dc90fcfc69a5e72a3e7aca078420
diogosilva <1250882@isep.ipp.pt>
2026-05-14 12:35:14 +0100

    Header da Conta Poupança

---
+++ Conta/Tipos de Conta/ContaPoupanca.h
@@ -0,0 +1,19 @@
+#ifndef PROJETOFSOFT_CONTAPOUPANCA_H
+#define PROJETOFSOFT_CONTAPOUPANCA_H
+
+#include "Conta.h"
+#include "../Conta.h"
+
+class ContaPoupanca : public Conta {
+
+public:
+
+    ContaPoupanca(const std::string& numConta, const std::string& pin);
+
+    bool depositar(double valor);
+    void consultarSaldo() const;
+
+    void mostrarInfo() const;
+};
+
+#endif //PROJETOFSOFT_CONTAPOUPANCA_H
