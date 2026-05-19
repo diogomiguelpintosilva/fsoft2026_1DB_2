59b427c336ee97ff63cf21c2a3d9f5fb72ac9535
diogosilva <1250882@isep.ipp.pt>
2026-05-19 16:22:50 +0100

    Criação dos ficheiros: - ViewUtils.h - ContaPoupancaService.cpp

--- Project/Headers/Views/ViewUtils.h
+++ Project/Headers/Views/ViewUtils.h
@@ -1,8 +1,26 @@
-//
-// Created by diogosilva on 5/18/26.
-//
-
 #ifndef TRABALHO_DE_SISTEMA_BANCÁRIO_VIEWUTILS_H
 #define TRABALHO_DE_SISTEMA_BANCÁRIO_VIEWUTILS_H
+
+#include <string>
+#include <limits>
+#include <iostream>
+
+
+inline void limparInput() {
+
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+inline void pausar() {
+
+    std::cout << " \n Prima ENTER para continuar ...";
+    std::cin.get();
+}
+
+inline void cabecalho(const std::string& titulo) {
+    std::cout << "\n =======================\n";
+    sdt::cout << " " << titulo << "\n";
+    std::cout << "=======================\n";
+}
 
 #endif //TRABALHO_DE_SISTEMA_BANCÁRIO_VIEWUTILS_H
--- Project/Sources/Services/ContaPoupancaService.cpp
+++ Project/Sources/Services/ContaPoupancaService.cpp
@@ -1,3 +1,53 @@
-//
-// Created by diogosilva on 5/18/26.
-//
+#include "service/ContaPoupancaService.h"
+#include <iostream>
+
+
+ContaPoupancaService::ContaPoupancaService(){}
+
+ContaPoupanca* ContaPoupancaService::criarConta(ContaOrdem* contaOrdem, const std::string& pin) {
+
+    if (contaOrdem == nullptr) {
+
+        std::cout "[ERRO] Conta Ordem invalida.\n";
+        return nullptr;
+    }
+    contaOrdem->adicionarContaPoupanca(pin);
+
+    return contaOrdem->getContaPoupanca(contaOrdem->numContasPoupanca() - 1);
+}
+
+
+ContaPoupanca* ContaPoupancaService::acederConta(ContaOrdem* contaOrdem, int indice, const std::string& pin) {
+
+    if (contaOrdem == nullptr) {
+
+        std::cout << "[ERRO] Conta Ordem invalida.\n";
+        return nullptr;
+    }
+    ContaPoupanca* cp = contaOrdem->getContaPoupanca(indice);
+
+    if (cp == nullptr) {
+
+        std::cout << "[ERRO] Conta Poupanca nao encontrada.\n";
+        return nullptr;
+    }
+
+    if (!cp -> verificarPin(pin)) {
+
+        std::cout << "[ERRO] PIN incorreto!\n";
+        return nullptr;
+    }
+
+    return cp;
+}
+
+
+bool ContaPoupancaService::depositar(ContaPoupanca* conta, double valor) {
+
+    if (conta == nullptr) {
+
+        std::cout << "[ERRO] Conta incalida.\n";
+        return false;
+    }
+    return conta -> depositar(valor);
+}
