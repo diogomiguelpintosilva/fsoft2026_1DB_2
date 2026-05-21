1e2d6233ddd0b24ee63993d373c1fbbaf3078bbc
diogosilva <1250882@isep.ipp.pt>
2026-05-21 09:14:45 +0100

    Organização de código

--- Project/Headers/Views/ViewContaOrdem.h
+++ Project/Headers/Views/ViewContaOrdem.h
@@ -5,22 +5,18 @@
 #include "controller/ContaPoupancaController.h"
 #include "view/ViewContaPoupanca.h"
 
-// A ViewContaOrdem trata de mostrar o menu da Conta Ordem
-// e de passar os dados ao ContaOrdemController.
-
 class ViewContaOrdem {
 public:
-    ViewContaOrdem(ContaOrdemController& contaOrdemController,
-                   ContaPoupancaController& contaPoupancaController);
+    ViewContaOrdem(ContaOrdemController& contaOrdemController,ContaPoupancaController& contaPoupancaController);
 
-    // Mostra o menu da Conta Ordem e fica em ciclo ate o utilizador sair.
+
     void mostrarMenu(ContaOrdem* conta);
 
 private:
     ContaOrdemController& contaOrdemController;
     ContaPoupancaController& contaPoupancaController;
 
-    // Cada funcao le os dados e chama o controller
+
     void opcaoDepositar(ContaOrdem* conta);
     void opcaoLevantar(ContaOrdem* conta);
     void opcaoTransferir(ContaOrdem* conta);
