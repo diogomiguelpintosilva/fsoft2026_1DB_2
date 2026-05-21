e69ffe443dc0ee892e30c078beb7253040543c2f
1251491 <1251491@isep.ipp.pt>
2026-05-21 09:30:44 +0100

    Corrigido

--- Project/Headers/Views/ViewUtils.h
+++ Project/Headers/Views/ViewUtils.h
@@ -5,22 +5,19 @@
 #include <limits>
 #include <iostream>
 
-
 inline void limparInput() {
-
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
 
 inline void pausar() {
-
-    std::cout << " \n Prima ENTER para continuar ...";
+    std::cout << "\n Prima ENTER para continuar ...";
     std::cin.get();
 }
 
 inline void cabecalho(const std::string& titulo) {
     std::cout << "\n =======================\n";
-    sdt::cout << " " << titulo << "\n";
-    std::cout << "=======================\n";
+    std::cout << " " << titulo << "\n";
+    std::cout << " =======================\n";
 }
 
-#endif //VIEWUTILS_H
+#endif // VIEWUTILS_H
