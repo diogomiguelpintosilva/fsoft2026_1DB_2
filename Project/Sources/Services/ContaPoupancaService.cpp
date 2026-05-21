a5fcca58b4c67e5f476a47a978ff139288e273ac
1251491 <1251491@isep.ipp.pt>
2026-05-21 09:32:09 +0100

    Corrigido

--- Project/Sources/Services/ContaPoupancaService.cpp
+++ Project/Sources/Services/ContaPoupancaService.cpp
@@ -1,53 +1,38 @@
 #include "service/ContaPoupancaService.h"
 #include <iostream>
 
-
-ContaPoupancaService::ContaPoupancaService(){}
+ContaPoupancaService::ContaPoupancaService() {}
 
 ContaPoupanca* ContaPoupancaService::criarConta(ContaOrdem* contaOrdem, const std::string& pin) {
-
     if (contaOrdem == nullptr) {
-
-        std::cout "[ERRO] Conta Ordem invalida.\n";
+        std::cout << "  [ERRO] Conta Ordem invalida.\n";
         return nullptr;
     }
     contaOrdem->adicionarContaPoupanca(pin);
-
     return contaOrdem->getContaPoupanca(contaOrdem->numContasPoupanca() - 1);
 }
 
-
 ContaPoupanca* ContaPoupancaService::acederConta(ContaOrdem* contaOrdem, int indice, const std::string& pin) {
-
     if (contaOrdem == nullptr) {
-
-        std::cout << "[ERRO] Conta Ordem invalida.\n";
+        std::cout << "  [ERRO] Conta Ordem invalida.\n";
         return nullptr;
     }
     ContaPoupanca* cp = contaOrdem->getContaPoupanca(indice);
-
     if (cp == nullptr) {
-
-        std::cout << "[ERRO] Conta Poupanca nao encontrada.\n";
+        std::cout << "  [ERRO] Conta Poupanca nao encontrada.\n";
         return nullptr;
     }
-
-    if (!cp -> verificarPin(pin)) {
-
-        std::cout << "[ERRO] PIN incorreto!\n";
+    if (!cp->verificarPin(pin)) {
+        std::cout << "  [ERRO] PIN incorreto!\n";
         return nullptr;
     }
-
     return cp;
 }
 
-
 bool ContaPoupancaService::depositar(ContaPoupanca* conta, double valor) {
-
     if (conta == nullptr) {
-
-        std::cout << "[ERRO] Conta incalida.\n";
+        std::cout << "  [ERRO] Conta invalida.\n";
         return false;
     }
-    return conta -> depositar(valor);
+    return conta->depositar(valor);
 }
