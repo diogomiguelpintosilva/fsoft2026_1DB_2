d1c9f5fa9712c957de62b24c1001a22a6d60a9e3
1251491 <1251491@isep.ipp.pt>
2026-05-21 09:33:39 +0100

    Corrigido

--- Project/Sources/Views/ViewPrincipal.cpp
+++ Project/Sources/Views/ViewPrincipal.cpp
@@ -2,48 +2,49 @@
 #include "view/ViewUtils.h"
 #include <iostream>
 
-ViewPrincipal::ViewPrincipal(ClienteController& clienteController, ViewCliente& viewCliente: clienteController(clienteController), viewCliente(viewCliente)){}
+ViewPrincipal::ViewPrincipal(ClienteController& clienteController, ViewCliente& viewCliente)
+    : clienteController(clienteController), viewCliente(viewCliente) {}
 
 void ViewPrincipal::iniciar(const std::string& nomeGestor) {
-    cabecalho("Bem-vindo ao" +nomeGestor);
+    cabecalho("Bem-vindo ao " + nomeGestor);
 
     int opcao;
     do {
-        std cout<< "\n 1. Registar novo cliente\n";
-        std cout<< "\n 2. Iniciar sessão\n";
-        std cout<< "\n 0. Sair\n";
-        std cout<< "\n Opcao: ";
-        std::cin>>opcao;
+        std::cout << "\n  1. Registar novo cliente\n";
+        std::cout << "  2. Iniciar sessao\n";
+        std::cout << "  0. Sair\n";
+        std::cout << "  Opcao: ";
+        std::cin >> opcao;
         limparInput();
 
         switch (opcao) {
-            case 1: opçaoRegistar(); break;
-            case 2: opcaoLogin(); break;
-            case 0: std::cout<<"\n Até breve!\n\n";break;
-            default: std::cout<<"Opcao invalida.\n";break;
+            case 1: opcaoRegistar(); break;
+            case 2: opcaoLogin();    break;
+            case 0: std::cout << "\n  Ate breve!\n\n"; break;
+            default: std::cout << "  Opcao invalida.\n"; break;
         }
-    }while(opcao!=0);
+    } while (opcao != 0);
 }
 
 void ViewPrincipal::opcaoRegistar() {
     std::string nome, nif, password;
-    std::cout<<"Nome: ";
-    std::getline(std::cin,nome);
-    std::cout<<"Nif: ";
-    std::getline(std::cin,nif);
-    std::cout<<"Password: ";
-    std::getline(std::cin,password);
+    std::cout << "  Nome: ";
+    std::getline(std::cin, nome);
+    std::cout << "  NIF: ";
+    std::getline(std::cin, nif);
+    std::cout << "  Password: ";
+    std::getline(std::cin, password);
 
     clienteController.registar(nome, nif, password);
     pausar();
 }
 
 void ViewPrincipal::opcaoLogin() {
-    std::string nif,password;
-    std::cout<<"NIF: ";
-    std::getline(std::cin,nif);
-    std::cout<<"Password: ";
-    std::getline(std::cin,password);
+    std::string nif, password;
+    std::cout << "  NIF: ";
+    std::getline(std::cin, nif);
+    std::cout << "  Password: ";
+    std::getline(std::cin, password);
 
     Cliente* cliente = clienteController.autenticar(nif, password);
     if (cliente != nullptr) {
