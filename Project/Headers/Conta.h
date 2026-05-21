6314cf3695a09650100ac7536bdf37d2f60f7bb7
1251491 <1251491@isep.ipp.pt>
2026-05-21 09:27:01 +0100

    Corrigido

--- Project/Headers/Conta.h
+++ Project/Headers/Conta.h
@@ -4,29 +4,23 @@
 #include <string>
 
 class Conta {
-
-    public:
-
+public:
     Conta(const std::string& numeroConta, const std::string& pin);
     virtual ~Conta() = default;
 
-    const std::string&getNumeroConta() const;
+    const std::string& getNumeroConta() const;
     double getSaldo() const;
 
-    bool verificarPin(const std::string &pinIntroduzi) const;
+    bool verificarPin(const std::string& pinIntroduzi) const;
 
     void setSaldo(double novoSaldo);
 
-    bool verificaPin(const std::string& pin) const;
     virtual void mostrarInfo() const = 0;
 
 protected:
-
     std::string numeroConta;
     double saldo;
     std::string pin;
-
-    void getSaldo(double novoSaldo);
 };
 
-#endif //CONTA_H
+#endif // CONTA_H
