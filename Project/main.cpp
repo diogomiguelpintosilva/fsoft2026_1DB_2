52ee5eb6dcfcc07e94fe00e6bcf80c836f8c48ef
1251491 <1251491@isep.ipp.pt>
2026-05-21 09:34:18 +0100

    Corrigido

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,46 +1,38 @@
 #include "GestorSistemaBancario.h"
 
-#include "service/ClienteService.h
-#include "service/GerenciamentoService.h"
 #include "service/ClienteService.h"
 #include "service/ContaOrdemService.h"
 #include "service/ContaPoupancaService.h"
-
+#include "service/GerenciamentoService.h"
 
 #include "controller/ClienteController.h"
 #include "controller/ContaOrdemController.h"
 #include "controller/ContaPoupancaController.h"
 #include "controller/GerenciamentoController.h"
 
-
+#include "view/ViewPrincipal.h"
 #include "view/ViewCliente.h"
 #include "view/ViewContaOrdem.h"
 #include "view/ViewContaPoupanca.h"
-#include "view/ViewPrincipal.h"
-
 
 int main() {
-
     GestorSistemaBancario gestor("Banco ISEP");
 
-
-    ClienteService clienteService(gestor);
-    ContaOrdemService contaOrdemService(gestor);
+    ClienteService       clienteService(gestor);
+    ContaOrdemService    contaOrdemService(gestor);
     ContaPoupancaService contaPoupancaService;
     GerenciamentoService gerenciamentoService;
 
-
-    ClienteController clienteController(clienteService);
+    ClienteController       clienteController(clienteService);
+    ContaOrdemController    contaOrdemController(contaOrdemService);
     ContaPoupancaController contaPoupancaController(contaPoupancaService);
-    ContaOrdemController contaOrdemController(contaOrdemService);
-    GerenciamentoController gerenciamentoController(gestor);
+    GerenciamentoController gerenciamentoController(gerenciamentoService);
 
-
-    ViewContapoupanca viewContaPoupanca(contaPoupancaController);
-    ViewContaOrdem viewContaOrdem(contaOrdemController, contaPoupancaController);
-    ViewCliente viewCliente(clienteController, contaOrdemController, gerenciamentoController, viewContaOrdem);
-    ViewPrincipal viewPrincipal(clienteController, viewCliente);
-
+    ViewContaPoupanca viewContaPoupanca(contaPoupancaController);
+    ViewContaOrdem    viewContaOrdem(contaOrdemController, contaPoupancaController);
+    ViewCliente       viewCliente(clienteController, contaOrdemController,
+                                  gerenciamentoController, viewContaOrdem);
+    ViewPrincipal     viewPrincipal(clienteController, viewCliente);
 
     viewPrincipal.iniciar(gestor.getNome());
 
