c5e5ff02370371b7a6deb2ffcb950f73a58d1577
1251491 <1251491@isep.ipp.pt>
2026-05-21 09:32:37 +0100

    Corrigido

--- Project/Sources/Controllers/ClienteController.cpp
+++ Project/Sources/Controllers/ClienteController.cpp
@@ -1,12 +1,16 @@
 #include "controller/ClienteController.h"
 
-ClienteContorller::ClienteController (ClienteSevice& clienteService):clienteService(clienteService){}
+ClienteController::ClienteController(ClienteService& clienteService)
+    : clienteService(clienteService) {}
 
-bool ClienteController::registar(const std::string& nome, const std::string$ nif, const std::string& password) {
+bool ClienteController::registar(const std::string& nome,
+                                  const std::string& nif,
+                                  const std::string& password) {
     return clienteService.registarCliente(nome, nif, password);
 }
 
-Cliente* ClienteController::autenticar (const std::string& nif, const std::string& password) {
+Cliente* ClienteController::autenticar(const std::string& nif,
+                                        const std::string& password) {
     return clienteService.autenticarCliente(nif, password);
 }
 
