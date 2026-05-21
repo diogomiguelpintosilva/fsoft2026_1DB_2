0a0109437af1b9a3ec7fddd1ae5a635ddf3faff7
1251491 <1251491@isep.ipp.pt>
2026-05-21 09:30:21 +0100

    Corrigido

--- Project/Headers/Views/ViewPrincipal.h
+++ Project/Headers/Views/ViewPrincipal.h
@@ -1,4 +1,4 @@
-#indef VIEWPRINCIPAL_H
+#ifndef VIEWPRINCIPAL_H
 #define VIEWPRINCIPAL_H
 
 #include "controller/ClienteController.h"
@@ -7,12 +7,15 @@
 class ViewPrincipal {
 public:
     ViewPrincipal(ClienteController& clienteController, ViewCliente& viewCliente);
+
+    void iniciar(const std::string& nomeGestor);
+
 private:
-    ViewCliente& viewCliente;
     ClienteController& clienteController;
+    ViewCliente& viewCliente;
 
     void opcaoRegistar();
     void opcaoLogin();
 };
 
-#endif
+#endif // VIEWPRINCIPAL_H
