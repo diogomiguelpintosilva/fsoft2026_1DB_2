006c12d4d2eba180130165a59d23d873021acbfc
mc <1251390@isep.ipp.pt>
2026-05-21 09:29:36 +0100

    Gerenciamento.cpp Gerenciamento.h GerenciamentoService.h

---
+++ Project/Sources/Controllers/ClienteController.cpp
@@ -0,0 +1,15 @@
+#include "controller/ClienteController.h"
+
+ClienteContorller::ClienteController (ClienteSevice& clienteService):clienteService(clienteService){}
+
+bool ClienteController::registar(const std::string& nome, const std::string$ nif, const std::string& password) {
+    return clienteService.registarCliente(nome, nif, password);
+}
+
+Cliente* ClienteController::autenticar (const std::string& nif, const std::string& password) {
+    return clienteService.autenticarCliente(nif, password);
+}
+
+Cliente* ClienteController::buscarPorNif(const std::string& nif) {
+    return clienteService.buscarPorNif(nif);
+}
