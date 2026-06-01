9a2c6b7a27622e392e1ae8dd9b262215fc5efb02
diogosilva <1250882@isep.ipp.pt>
2026-06-01 17:23:16 +0100

    Correção da ClienteService.cpp

--- Project/Sources/Services/ClienteService.cpp
+++ Project/Sources/Services/ClienteService.cpp
@@ -1,25 +1,18 @@
 #include "ClienteService.h"
-#include <iostream>
 
-ClienteService::ClienteService(GestorSistemaBancario& gestor)
-    : gestor(gestor) {}
+ClienteService::ClienteService(GestorSistemaBancario& gestor) : gestor(gestor) {}
 
 bool ClienteService::registarCliente(const std::string& nome, const std::string& nif, const std::string& password) {
     if (gestor.existeNif(nif)) {
-        std::cout << "  [ERRO] Ja existe um cliente com o NIF " << nif << ".\n";
         return false;
     }
     gestor.registarCliente(nome, nif, password);
     return true;
 }
 
-Cliente* ClienteService::autenticarCliente(const std::string& nif,
-                                           const std::string& password) {
-    Cliente* cliente = gestor.autenticarCliente(nif, password);
-    if (cliente == nullptr) {
-        std::cout << "  [ERRO] NIF ou password incorretos.\n";
-    }
-    return cliente;
+Cliente* ClienteService::autenticarCliente(const std::string& nif, const std::string& password) {
+    return gestor.autenticarCliente(nif, password);
+
 }
 
 Cliente* ClienteService::buscarPorNif(const std::string& nif) {
